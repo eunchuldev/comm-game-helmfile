ba943ef1ec75bdb275857ff3e838422d8d940f2f
Eunchul <eunchul.dev@gmail.com>
2021-02-05 19:58:59 +0900

    [fix] kubefwd context aware

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -1,2 +1,2 @@
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )"
-sudo -E $ROOT/scripts/kubefwd svc -n default
+sudo -E $ROOT/scripts/kubefwd svc -n default -x $KUBE_CONTEXT
