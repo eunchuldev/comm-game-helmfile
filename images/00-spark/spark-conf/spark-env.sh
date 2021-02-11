4227ce9195f19ed9afe0e29ea293d8d1d75609ee
Eunchul Song <eunchulsong9@gmail.com>
2021-02-12 02:09:33 +0900

    [fix] remove spark jmx exporter

--- images/00-spark/spark-conf/spark-env.sh
+++ images/00-spark/spark-conf/spark-env.sh
@@ -6,8 +6,8 @@ spark.driver.host $(hostname -i)
 spark.kubernetes.driver.pod.name ${POD_NAME:-$(hostname)}
 spark.kubernetes.namespace $(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 spark.kubernetes.container.image ${SPARK_EXECUTOR_IMAGE:-"song9446/spark:3.0.1-hadoop-3.3.0"}
-spark.driver.extraJavaOptions -javaagent:$SPARK_HOME/jars/jmx_prometheus_javaagent-0.11.0.jar=9091:$SPARK_HOME/conf/prometheus.yaml
-spark.executor.extraJavaOptions -javaagent:$SPARK_HOME/jars/jmx_prometheus_javaagent-0.11.0.jar=9091:$SPARK_HOME/conf/prometheus.yaml
+#spark.driver.extraJavaOptions -javaagent:$SPARK_HOME/jars/jmx_prometheus_javaagent-0.11.0.jar=9091:$SPARK_HOME/conf/prometheus.yaml
+#spark.executor.extraJavaOptions -javaagent:$SPARK_HOME/jars/jmx_prometheus_javaagent-0.11.0.jar=9091:$SPARK_HOME/conf/prometheus.yaml
 #spark.kubernetes.executor.annotation.prometheus.io/scrape true
 #spark.kubernetes.executor.annotation.prometheus.io/port 9091
 #spark.kubernetes.executor.annotation.prometheus.io/path /
